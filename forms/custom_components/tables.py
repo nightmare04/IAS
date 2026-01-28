e92af839ac2cd55f43793f42a226cdd9adda9cc5
Mikhail Malinichev <04nightmare86@gmail.com>
2026-01-28 22:18:38 +0900

    update table

--- forms/custom_components/tables.py
+++ forms/custom_components/tables.py
@@ -1,41 +1,50 @@
-from PyQt6.QtCore import QAbstractTableModel, QModelIndex, Qt
-from PyQt6.QtGui import QFont, QBrush, QColor
+from PyQt6.QtCore import Qt, QModelIndex, QAbstractTableModel
+from PyQt6.QtGui import QBrush, QColor
 from PyQt6.QtWidgets import QTableView, QAbstractItemView
 
 
 class GroupTableModel(QAbstractTableModel):
-    def __init__(self, data, headers, parent=None):
+    def __init__(self, data=None, headers=None, parent=None):
         super().__init__(parent)
-        self._headers = headers[1:]  # Исключаем первый заголовок
-        self._raw_data = data
+        self._headers = headers or []
         self._prepared_data = []
-        self._group_rows = []  # Индексы строк с группами
-        self._group_values = []  # Значения групп
-        self._prepare_data()
+        self._group_rows = []
+        self._group_values = []
+        if data:
+            self.load_data(data)
 
-    def _prepare_data(self):
-        """Подготавливает данные с группами"""
-        # Сортируем по первому столбцу для группировки
-        sorted_data = sorted(self._raw_data, key=lambda x: str(x[0]))
+    def load_data(self, data):
+        """Загружает данные из запроса Peewee"""
+        self.beginResetModel()
+
+        self._prepared_data = []
+        self._group_rows = []
+        self._group_values = []
+
+        # Сортируем по категории
+        sorted_data = sorted(data, key=lambda x: str(x.category.name))
 
         current_group = None
         row_idx = 0
 
-        for row in sorted_data:
-            group_value = str(row[0])
+        for item in sorted_data:
+            group_value = str(item.category.name)
 
             # Если началась новая группа
             if group_value != current_group:
                 # Добавляем строку группы
-                self._prepared_data.append([''] * (len(self._headers)))  # Пустая строка для группы
+                self._prepared_data.append([''] * (len(self._headers)))
                 self._group_rows.append(row_idx)
                 self._group_values.append(group_value)
                 row_idx += 1
                 current_group = group_value
 
-            # Добавляем обычную строку данных без первого столбца
-            self._prepared_data.append(row[1:])  # Исключаем первый элемент
+            # Добавляем обычную строку данных
+            row_data = [item.name, item.color, str(item.price)]
+            self._prepared_data.append(row_data)
             row_idx += 1
+
+        self.endResetModel()
 
     def rowCount(self, parent=QModelIndex()):
         return len(self._prepared_data)
@@ -53,13 +62,11 @@ class GroupTableModel(QAbstractTableModel):
         if 0 <= row < len(self._prepared_data) and 0 <= col < len(self._headers):
             if role == Qt.ItemDataRole.DisplayRole:
                 if row in self._group_rows:
-                    # Для строк групп показываем значение группы в первой колонке
                     if col == 0:
                         group_index = self._group_rows.index(row)
                         return self._group_values[group_index]
-                    return ""  # Пустая строка для остальных колонок
+                    return ""
                 else:
-                    # Для обычных строк
                     value = self._prepared_data[row][col]
                     return str(value) if value is not None else ""
 
@@ -70,12 +77,15 @@ class GroupTableModel(QAbstractTableModel):
                 return font
 
             elif role == Qt.ItemDataRole.BackgroundRole and row in self._group_rows:
-                return QBrush(QColor(220, 220, 220))  # Светло-серый фон для групп
+                return QBrush(QColor(220, 220, 220))
 
             elif role == Qt.ItemDataRole.TextAlignmentRole and row in self._group_rows:
                 if col == 0:
                     return Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignVCenter
                 return Qt.AlignmentFlag.AlignCenter
+
+            elif role == Qt.ItemDataRole.ForegroundRole and row in self._group_rows:
+                return QBrush(QColor(0, 0, 139))  # Темно-синий цвет для групп
 
         return None
 
@@ -86,15 +96,7 @@ class GroupTableModel(QAbstractTableModel):
         return None
 
     def isGroupRow(self, row):
-        """Проверяет, является ли строка строкой группы"""
         return row in self._group_rows
-
-    def getGroupValue(self, row):
-        """Возвращает значение группы для строки"""
-        if row in self._group_rows:
-            index = self._group_rows.index(row)
-            return self._group_values[index]
-        return None
 
 
 class GroupingTableView(QTableView):
@@ -102,13 +104,11 @@ class GroupingTableView(QTableView):
         super().__init__(parent)
         self.setAlternatingRowColors(True)
         self.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
-        self.setSelectionMode(QAbstractItemView.SelectionMode.SingleSelection)
 
     def setSpanForGroups(self):
-        """Объединяет ячейки для строк групп"""
         model = self.model()
         if isinstance(model, GroupTableModel):
             for row in range(model.rowCount()):
                 if model.isGroupRow(row):
-                    # Объединяем все ячейки в строке группы
                     self.setSpan(row, 0, 1, model.columnCount())
+
