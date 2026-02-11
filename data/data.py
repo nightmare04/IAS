6079ee3a0be168fbe3810068034490af6b47aec2
Mikhail Malinichev <04nightmare86@gmail.com>
2026-02-11 14:20:56 +0900

    add back_ref, fix get_systems_for_plane

--- data/data.py
+++ data/data.py
@@ -5,19 +5,26 @@ from peewee import *
 
 db = SqliteDatabase('./data/database.db', pragmas={'foreign_keys': 1})
 
-def get_systems_for_plane(plane:PlaneBase) -> list[PlaneSystemBase]:
+def get_systems_for_plane(plane:PlaneBase, group:GroupBase=None) -> list[PlaneSystemBase]:
     osob_list = OsobPlaneBase.select().where(OsobPlaneBase.plane == plane)
-    systems = (PlaneSystemBase.select()
+    systems = (PlaneSystemBase
+               .select()
+               .where(PlaneSystemBase.group == group)
                .where(PlaneSystemBase.id.not_in(OsobSystemAddBase
-                      .select(OsobSystemAddBase.system)))
+                                                .select(OsobSystemAddBase.system)
+                                                .where(OsobSystemAddBase.system.plane_type == plane.id)))
                .where(PlaneSystemBase.id.not_in(OsobSystemRemoveBase
-                      .select(OsobSystemRemoveBase.system)
-                      .where(OsobSystemRemoveBase.osob << osob_list)))
+                                                .select(OsobSystemRemoveBase.system)
+                                                .where(OsobSystemRemoveBase.osob << osob_list)))
+               .switch(PlaneSystemBase)
                .union(PlaneSystemBase
                       .select()
                       .join(OsobSystemAddBase)
+                      .where(OsobSystemAddBase.system.group == group.id)
                       .where(OsobSystemAddBase.osob << osob_list))
-                    )
+               )
+    if group:
+        systems = systems.where(PlaneSystemBase.group == group)
     return systems
 
 class BaseModel(Model):
@@ -37,52 +44,52 @@ class PodrazdBase(BaseModel):
 
 
 class GroupBase(BaseModel):
-    plane_type = ForeignKeyField(PlaneTypeBase)
+    plane_type = ForeignKeyField(PlaneTypeBase, backref='groups')
     name = CharField(unique=True)
 
 
 class PlaneSystemBase(BaseModel):
-    plane_type = ForeignKeyField(PlaneTypeBase)
+    plane_type = ForeignKeyField(PlaneTypeBase, backref='systems')
     name = CharField(unique=True)
     group = ForeignKeyField(GroupBase)
 
 
 class AgregateBase(BaseModel):
-    system = ForeignKeyField(PlaneSystemBase)
+    system = ForeignKeyField(PlaneSystemBase, backref='agregates')
     count_on_plane = IntegerField(default=1)
     name = CharField(unique=False)
 
 
 class PlaneBase(BaseModel):
-    plane_type = ForeignKeyField(PlaneTypeBase)
-    podrazd = ForeignKeyField(PodrazdBase)
+    plane_type = ForeignKeyField(PlaneTypeBase, backref='planes')
+    podrazd = ForeignKeyField(PodrazdBase, backref='planes')
     zav_num = CharField(unique=True)
     bort_number = CharField(unique=False)
 
 
 class OsobBase(BaseModel):
-    plane_type = ForeignKeyField(PlaneTypeBase)
+    plane_type = ForeignKeyField(PlaneTypeBase, backref='osobs')
     name = CharField(unique=True)
 
 
 class OsobPlaneBase(BaseModel):
-    osob = ForeignKeyField(OsobBase)
+    osob = ForeignKeyField(OsobBase, backref='osobs')
     plane = ForeignKeyField(PlaneBase)
 
 
 class OsobSystemAddBase(BaseModel):
-    osob = ForeignKeyField(OsobBase)
-    system = ForeignKeyField(PlaneSystemBase)
+    osob = ForeignKeyField(OsobBase, backref='systems_to_add')
+    system = ForeignKeyField(PlaneSystemBase, backref='systems_to_add')
 
 
 class OsobSystemRemoveBase(BaseModel):
-    osob = ForeignKeyField(OsobBase)
-    system = ForeignKeyField(PlaneSystemBase)
+    osob = ForeignKeyField(OsobBase, backref='systems_to_remove')
+    system = ForeignKeyField(PlaneSystemBase, backref='systems_to_remove')
 
 
 class OtkazAgregateBase(BaseModel):
-    agregate = ForeignKeyField(AgregateBase)
-    plane = ForeignKeyField(PlaneBase)
+    agregate = ForeignKeyField(AgregateBase, backref='otkaz_agregates')
+    plane = ForeignKeyField(PlaneBase , backref='otkaz_agregates')
     description = CharField(unique=False, default='')
     number = CharField()
     removed = BooleanField(default=False)
